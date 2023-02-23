f8e6f8e74133918165a6bdcdfa7c2eb78d0e3251
[Jan Dressel] <[jan@dressel.cloud]>
2023-02-23 10:04:20 +0100

    [first docfile to commit]

---
+++ ffg/ffg_club/doctype/certificates/certificates.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2023, Dressel and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Certificates(Document):
+	pass
---
+++ ffg/ffg_club/doctype/members/members.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2023, Dressel and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Members', {
+	// refresh: function(frm) {
+
+	// }
+});
